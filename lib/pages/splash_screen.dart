f516358e85b1cc54724e8c75d685cea0e2068563
bukhanovskayamarina22 <bukhanovskayamarina22@protonmail.com>
2024-02-22 13:11:10 +0300

    splash screen (with flutter logo)

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'get_started_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => GetStartedPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo(
+              size: 150,
+            ),
+            PaddingTop24(),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
