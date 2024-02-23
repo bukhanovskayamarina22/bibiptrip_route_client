8b05e570d7de3ea53a2ef0dcccd36718cad3fc86
bukhanovskayamarina22 <bukhanovskayamarina22@protonmail.com>
2024-02-23 10:18:21 +0300

    added more paddings

--- lib/widgets/paddings.dart
+++ lib/widgets/paddings.dart
@@ -16,6 +16,38 @@ class PaddingAll24 extends StatelessWidget {
   }
 }
 
+class PaddingLeft24 extends StatelessWidget {
+  final Widget? child;
+  const PaddingLeft24({
+    super.key, this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 24),
+      child: child,
+    );
+  }
+}
+
+class PaddingAll12 extends StatelessWidget {
+  final Widget? child;
+  const PaddingAll12({
+    super.key,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: child,
+    );
+  }
+}
+
+
 class PaddingTop8 extends StatelessWidget {
   final Widget? child;
   const PaddingTop8({
