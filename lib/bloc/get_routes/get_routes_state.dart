d165872f2c2946f23c9f64221cb800c7809284d8
bukhanovskayamarina22 <bukhanovskayamarina22@protonmail.com>
2024-02-26 12:27:21 +0300

    made states equatable

--- lib/bloc/get_routes/get_routes_state.dart
+++ lib/bloc/get_routes/get_routes_state.dart
@@ -1,17 +1,30 @@
 part of 'get_routes_bloc.dart';
 
-sealed class GetRoutesState {}
 
-final class GetRoutesInitial extends GetRoutesState {} 
-final class GetRoutesLoading extends GetRoutesState {} 
+sealed class GetRoutesState extends Equatable {}
+
+final class GetRoutesInitial extends GetRoutesState {
+  @override
+  List<Object?> get props => [];
+} 
+final class GetRoutesLoading extends GetRoutesState {
+  @override
+  List<Object?> get props => [];
+} 
 final class GetRoutesSuccess extends GetRoutesState {
   final List<RouteViewModel> routes;
 
   GetRoutesSuccess(this.routes);
+  
+  @override
+  List<Object?> get props => [routes];
 
 } 
 final class GetRoutesFailure extends GetRoutesState {
   final String errorMessage;
 
   GetRoutesFailure(this.errorMessage);
+  
+  @override
+  List<Object?> get props => [errorMessage];
 } 
